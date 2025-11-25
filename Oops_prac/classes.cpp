d6f794a4af10692a61d009b68023d6e936503a9a
yashrawat <yashrawat682@gmail.com>
2025-11-25 22:06:43 +0530

    learned how to allocate the memory statically

--- Oops_prac/classes.cpp
+++ Oops_prac/classes.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 
+#include"student.cpp"
+
 using namespace std;
+
 
 
 
@@ -17,13 +20,26 @@ class Hero{
         level=l;
     }
 };
+
+
 int main(){
 
 
+    cout<<sizeof(Student)<<endl;
+
+    // this is called statically allocated memory .
+    
     Hero h1= Hero(2,"yash",4);
 
+    cout<<"Size of the class is :"<<sizeof(Hero)<<endl;
 
     cout<<h1.health<<" "<<h1.str<<" "<<h1.level<<endl;
+
+    cout<<"This is Student class"<<endl;
+
+    Student s1=Student("yash");
+
+    cout<<"Student is "<<s1.name<<endl;
 
 
 
---
+++ Oops_prac/student.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+
+using namespace std;
+
+
+
+
+
+
+
+
+class Student{
+
+
+
+    public:
+
+    string name ;
+
+    Student(string n){
+        name =n;
+    }
+
+};
