01ad93092f5aba7a9198136a223be22e38af0a95
yashrawat <yashrawat682@gmail.com>
2025-11-29 06:50:09 +0530

    understanding the shallow and deep copy

--- Oops_prac/copy_constructor.cpp
+++ Oops_prac/copy_constructor.cpp
@@ -5,10 +5,12 @@ class Hero {
 public:
     int level;
     string name;
+    int *power;
 
-    Hero(int l, string s) {
+    Hero(int l, string s,int *p) {
         level = l;
         name = s;
+        power=p;
 
         cout<<this<<endl;
     }
@@ -17,22 +19,30 @@ public:
         cout<<"this is new copy constructor"<<endl;
         this->level=h.level;
         this->name=h.name;
+        this->power=h.power;
     }
 };
 
-ostream &operator <<(ostream &os,Hero h){
-    os<<"("<<h.level<<" "<<h.name<<" )";
+ostream &operator <<(ostream &os,const Hero &h){
+    os<<"("<<h.level<<" "<<h.name<<" "<<*(h.power)<<" )";
     return os;
 }
 
 int main() {
-    Hero *h1=new Hero(3,"yash");
+    int power=10;
+    int *p=&power;
+    Hero *h1=new Hero(3,"yash",p);
 
+int newPower=20;
 
+    h1->power=&newPower;
 
     Hero *h2=new Hero(*h1);
 
+    
+
     cout<<*h1<<endl;
+
 
     cout<<"Printing the Object h2"<<endl;
 
